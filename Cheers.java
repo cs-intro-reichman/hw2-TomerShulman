a6150336d2acbf0482ab9e719b3ffc30969757f5
TomerShulman <tomer.shulman@post.runi.ac.il>
2024-11-16 16:07:35 +0200

    test

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,27 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String Name = args[0];
+            int Times = Integer.parseInt(args[1]);
+            String whenAN = "aefhilmnorsxAEFHILMNORSX";
+            int i=0;
+            while (i < Name.length()) {
+                    if (whenAN.indexOf(Name.charAt(i))!=-1){
+                        System.out.println("Give me an "+ Name.charAt(i)+": "+Name.charAt(i)+"!");
+                        
+                        }
+                    else{
+                        System.out.println("Give me a "+ Name.charAt(i)+": "+Name.charAt(i)+"!");
+                        
+                    }
+                    i++;
+            }
+        
+            System.out.println("What does that spell?");
+            for (int k = 0; k < Times; k++) {
+                System.out.println(Name+ "! ! !");
+            }
+        
+
         }
 }
