1aee4922c86a1b5ead86b50ac19656a748e5aa16
TomerShulman <tomer.shulman@post.runi.ac.il>
2024-11-17 12:59:35 +0200

    test

--- Collatz.java
+++ Collatz.java
@@ -7,12 +7,12 @@ public class Collatz {
 		int count=1;
 		for (int seed = 1; seed < HigherNum+1; seed++) {
 			Last=seed;
-			if(Mode.equals("V")==true){
+			if(Mode.equals("v")==true){
 			System.out.print(Last);
 			}
 			if (seed==1){
 				Last= Last*3+1;
-				if(Mode.equals("V")==true){
+				if(Mode.equals("v")==true){
 				System.out.print(", "+Last);
 				}
 				count++;
@@ -20,20 +20,20 @@ public class Collatz {
 			while(Last != 1){
 				if(Last%2==0){
 					Last /= 2;
-					if(Mode.equals("V")==true){
+					if(Mode.equals("v")==true){
 					System.out.print(", "+Last);
 					}
 					count++;
 				}
 				else{
 					Last= Last*3+1;
-					if(Mode.equals("V")==true){
+					if(Mode.equals("v")==true){
 					System.out.print(", "+Last);
 					}
 					count++;
 				}
 			}
-				if(Mode.equals("V")==true){
+				if(Mode.equals("v")==true){
 				System.out.println(" ("+count+")");
 				}
 				count=1;
